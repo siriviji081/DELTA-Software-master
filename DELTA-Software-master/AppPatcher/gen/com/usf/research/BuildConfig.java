f8f6a300416f9f252b0c96bc00a540049a80a0d7
siriviji081 <siriviji081@gmail.com>
2014-05-15 09:21:19 +0530

    Revert "0"
    This reverts commit 5905c94a7e050da853c632fb774410a91bf9989d.

--- DELTA-Software-master/AppPatcher/gen/com/usf/research/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.usf.research;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- DELTA-Software-master/AppPatcher/jni/bspatch.h
+++
@@ -1,6 +0,0 @@
-#ifndef BSPATCH_H
-#define BSPATCH_H
-
-extern int test(int a);
-
-#endif /* BSPATCH_H */
